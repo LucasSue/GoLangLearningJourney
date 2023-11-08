2ca04a098df14888a08de0ccdc3705d71c4b99cc
lucas <lucas@89trillion.com>
2023-11-08 14:42:50 +0800

    Hello Go

---
+++ gofile/Book.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type MyBook struct {
+	name string
+	num  int
+}
+
+func (m *MyBook) SetMybook(num int, name string) {
+	m.num = num
+	m.name = name
+}
+
+func (m *MyBook) ShowMyBook() {
+	fmt.Printf("My Book is %s,number is %d", m.name, m.num)
+}
+
+func main() {
+	mybook := MyBook{"su", 10}
+	mybook.ShowMyBook()
+}
